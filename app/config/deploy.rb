7ffedd12947be05c05190d3ce252f0e5a82d7cf3
Jesse Rosato <jesse.rosato@gmail.com>
2014-02-11 12:22:00 -0800

    Attempted fix for CSRF issue.

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -6,7 +6,7 @@ require 'capistrano/ext/multistage'
 set :application, "lists.io"
 set :domain,      "72.47.211.211"
 set :deploy_to,   "/var/www/vhosts/lists.io"
-set :deploy_via,  :rsync_with_remote_cache
+set :deploy_via,  :copy
 set :app_path,    "app"
 set :web_path,    "web"
 
@@ -25,15 +25,15 @@ set :scm,         :git
 set :model_manager, "doctrine"
 # Or: `propel`
 
-set :shared_files,      ["app/config/parameters.yml"]
+set :shared_files,        ["app/config/parameters.yml"]
 set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor", app_path + "/sessions"]
 set :dump_assetic_assets, true
 
 set :use_composer, true
 set :update_vendors, true
 
-role :web,        "lists.io"            # Your HTTP server, Apache/etc
-role :app,        "lists.io/web", :primary => true       # This may be the same as your `Web` server
+role :web,        domain            # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  3
 
