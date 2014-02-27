188d86fd64127aa290dc80251d3338e2553de2f1
Jesse Rosato <jesse.rosato@gmail.com>
2014-02-26 16:32:09 -0800

    First go at Twitter login.

---
+++ src/ListsIO/Bundle/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jesserosato
+ * Date: 2/26/14
+ * Time: 10:14 AM
+ */
+
+namespace ListsIO\Bundle\UserBundle\Controller;
+
+use \FOS\UserBundle\Controller\SecurityController as BaseSecurityController;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends BaseSecurityController {
+
+} 
